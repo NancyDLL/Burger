c3cf8cd24567321c6b451c0b14c829e56a78c9d2
NancyDLL <nano14@mac.com>
2017-11-10 12:23:50 -0600

    updated server file to require packages

---
+++ server.js
@@ -0,0 +1,4 @@
+// Require the following npm packages inside of the server.js file:
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
