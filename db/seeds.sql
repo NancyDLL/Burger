95fb5919c66ac3641c84cf967da5f7f51c666b19
NancyDLL <nano14@mac.com>
2017-11-10 13:18:29 -0600

    created seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kobe Beef Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black & Blue Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoke Burger with Bacon', FALSE);
