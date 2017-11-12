0ad8b87a544bdb39407be527c5f9248c410f4cbb
NancyDLL <nano14@mac.com>
2017-11-12 16:25:43 -0600

    psuedocoded the models file.

---
+++ models/burger.js
@@ -0,0 +1,13 @@
+var orm = require("../config/orm.js");
+
+//PSUEDOCODE
+//This is where the function for calling the ORM
+//
+//orm.selectAll("burgers", function(res)
+//cb(res);
+//orm.insertOne("burgers", cols, vals, function(res)
+//cb(res);
+//orm.insertOne("burgers", cols, vals, function(res)
+//cb(res);
+
+module.exports = burger;
