08a5543a6b4d62e103b527b935c82f8d5c2c2117
NancyDLL <nano14@mac.com>
2017-11-12 16:05:10 -0600

    psuedocoded the ORM file

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require('./connection.js');
+
+//PSUEDOCODE
+//This is where I would add the functions to handle the queries:
+//A function to select from the DB table
+//A function to insert bergers into the DB table
+//A function to update the DB table
+
+module.exports = orm;
