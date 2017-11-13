7f32479f7c1a7927fb9668888867e14e13c632c3
NancyDLL <nano14@mac.com>
2017-11-13 15:33:54 -0600

    revised server.js

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ var bodyParser = require("body-parser");
 
 //Set up Express server
 var app = express();
-app.engine('')
 
-var port = 3000;
-app.listen(port);
+//Set up Port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory to be served
+app.use(express.static("public"));
+
+//Set up the Router
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+
+//Set up the Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
